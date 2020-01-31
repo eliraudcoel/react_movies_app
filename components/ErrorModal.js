375b506f6541a86da9ac4fb41700ae5d01b4cba5
eliraudcoel <emmanuellecoel@yahoo.fr>
2020-01-31 18:32:39 +0100

    Improve Error modal display

--- components/ErrorModal.js
+++ components/ErrorModal.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
-import { Overlay } from 'react-native-elements';
+import { Text, View } from 'react-native';
+import { Overlay, Button, Icon } from 'react-native-elements';
+import Colors from '../constants/Colors';
 
 export default function ErrorModal(props) {
     // States
@@ -10,16 +11,33 @@ export default function ErrorModal(props) {
         setVisible(visible);
     }, [visible]);
 
-    // TODO : improve display
     return (
         <Overlay
             isVisible={visible}
-            windowBackgroundColor="rgba(255, 255, 255, .5)"
-            overlayBackgroundColor="red"
+            windowBackgroundColor="rgba(0, 0, 0, .5)"
             width="auto"
             height="auto"
         >
-            <Text>Hello from Overlay!</Text>
+            <View style={{ width: 300, alignItems: "center", justifyContent: "center" }}>
+                <Icon
+                    type='material'
+                    size={70}
+                    name='error'
+                    color={Colors.tintColor}
+                    underlayColor={Colors.transparent}
+                    onPress={() => this.goBack()}
+                />
+                <View style={{ paddingTop: 20, paddingBottom: 20, alignItems: "center"}}>
+                    <Text style={{textAlign: "center", paddingBottom: 5, paddingTop: 5}}>Oups!</Text>
+                    <Text style={{textAlign: "center", paddingBottom: 5, paddingTop: 5}}>Votre session à expirer</Text>
+                    <Text style={{textAlign: "center", paddingBottom: 5, paddingTop: 5}}>Afin de profiter de notre service, veuillez vous identifier à nouveau.</Text>
+                </View>
+                <Button
+                    title="Compris"
+                    type="clear"
+                    onPress={() => setVisible(false)}
+                />
+            </View>
         </Overlay>
     );
 }
