7a64f9b462f59c97575ed50584036a09d7138831
eliraudcoel <emmanuellecoel@yahoo.fr>
2020-02-28 13:17:05 +0100

    Fix error message on signIn page

--- components/ErrorModal.js
+++ components/ErrorModal.js
@@ -32,10 +32,10 @@ export default function ErrorModal(props) {
                     underlayColor={Colors.transparent}
                     onPress={() => this.goBack()}
                 />
-                <View style={{ paddingTop: 20, paddingBottom: 20, alignItems: "center"}}>
-                    <Text style={{textAlign: "center", paddingBottom: 5, paddingTop: 5}}>Oups!</Text>
-                    <Text style={{textAlign: "center", paddingBottom: 5, paddingTop: 5}}>{error.title}</Text>
-                    <Text style={{textAlign: "center", paddingBottom: 5, paddingTop: 5}}>{error.body}</Text>
+                <View style={{ paddingTop: 20, paddingBottom: 20, alignItems: "center" }}>
+                    <Text style={{ textAlign: "center", paddingBottom: 5, paddingTop: 5 }}>Oups!</Text>
+                    <Text style={{ textAlign: "center", paddingBottom: 5, paddingTop: 5 }}>{error && error.title || ''}</Text>
+                    <Text style={{ textAlign: "center", paddingBottom: 5, paddingTop: 5 }}>{error && error.body || ''}</Text>
                 </View>
                 <Button
                     title="Compris"
